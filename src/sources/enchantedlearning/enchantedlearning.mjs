27bae0955c39bdda6172058f7806f8470950b934
Filipe <filipemeneses@users.noreply.github.com>
2023-03-18 08:50:47 -0300

    perf: scrap enchantedlearning

--- src/sources/enchantedlearning/enchantedlearning.mjs
+++ src/sources/enchantedlearning/enchantedlearning.mjs
@@ -7,44 +7,67 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const CATEGORIES_URL = 'https://www.enchantedlearning.com/wordlist/';
+const CATEGORIES_PATH = path.resolve(__dirname, 'cache/categories.json');
+const CATEGORIES_WORDS_PATH = path.resolve(__dirname, 'categories-words.json');
+
 const fallbackReadFile = async (path) => {
   try {
-    await fs.readFile(path);
+    return (await fs.readFile(path)).toString();
   } catch (e) {
     return '';
   }
 };
 
 const scrapCategories = async () => {
-  const CATEGORIES_URL = 'https://www.enchantedlearning.com/wordlist/';
-
   const categoriesHtml = (await axios.get(CATEGORIES_URL)).data;
   const $ = load(categoriesHtml);
 
   const categories = $('.page-list-item__title a').map((_, el) => ({
     name: snakeCase($(el).text()),
-    href: $(el).attr('href'),
+    href: `https://www.enchantedlearning.com${$(el).attr('href')}`,
   })).get();
 
   return categories;
 };
 
-const cachedScrapCategories = async () => {
-  const CATEGORIES_PATH = path.resolve(__dirname, 'cache/categories.json');
+const cachedScrapCategories = async (bust = false) => {
   const categoriesFile = await fallbackReadFile(CATEGORIES_PATH);
 
-  if (categoriesFile) return JSON.parse(categoriesFile);
+  if (categoriesFile && !bust) return JSON.parse(categoriesFile);
 
   const categories = await scrapCategories();
-  await fs.writeFile(CATEGORIES_PATH, JSON.stringify(categories));
+  await fs.writeFile(CATEGORIES_PATH, JSON.stringify(categories, null, 2));
 
   return categories;
 };
 
-const scrapAll = async () => {
-  const categories = await cachedScrapCategories();
+const scrapCategoryWords = async (url) => {
+  const categoriesHtml = (await axios.get(url)).data;
+  const $ = load(categoriesHtml);
 
-  console.log(categories);
+  return $('.wordlist-item').map((_, el) => (
+    snakeCase($(el).text()).replace(/_/g, ' ')
+  )).get();
+};
+
+const scrapAll = async () => {
+  const scrappedContent = await fallbackReadFile(CATEGORIES_WORDS_PATH);
+  if (scrappedContent) {
+    console.log(`"${CATEGORIES_WORDS_PATH}" is not empty.\nSkipping scrap ...`);
+    return;
+  }
+
+  const categories = await cachedScrapCategories(true);
+
+  const words = await Promise.all(
+    categories.map(async (category) => {
+      category.words = await scrapCategoryWords(category.href);
+      return category;
+    }),
+  );
+
+  await fs.writeFile(CATEGORIES_WORDS_PATH, JSON.stringify(words, null, 2));
 };
 
 const enchantedlearning = {
